ec9e784a101429f2401f6d917e04b9bb35360fff
xera51 <xera51@protonmail.com>
2021-06-15 15:09:18 -0400

    Runner is able to run any exercise between MIN_EXERCISE and MAX_EXERCISE, given it follows the file path oop.exercises.ex##.Application, ## being the exercise number

--- src/main/java/oop/exercises/main/Runner.java
+++ src/main/java/oop/exercises/main/Runner.java
@@ -1,10 +1,13 @@
 /*
  *  UCF COP3330 Summer 2021 Assignment 3 Solution
- *  Copyright 2021 first_name last_name
+ *  Copyright 2021 Christopher Gray
  */
 package oop.exercises.main;
 
 import oop.exercises.util.DataReader;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.function.IntPredicate;
 
 
@@ -13,33 +16,61 @@ public class Runner {
     private static final DataReader in = new DataReader();
     private static final int MIN_EXERCISE = 41;
     private static final int MAX_EXERCISE = 46;
-    private static final IntPredicate VALID_EXERCISE =
-            (x) -> MIN_EXERCISE <= x && x <= MAX_EXERCISE;
+    private static final IntPredicate VALIDATE_EXERCISE =
+            x -> MIN_EXERCISE <= x && x <= MAX_EXERCISE;
 
     public static void main(String[] args) {
         Runner runner = new Runner();
-        runner.runExercise(runner.readExercise());
+        do {
+            int exerciseNumber = runner.readExercise();
+            runner.runExercise(exerciseNumber);
+        } while(runner.runAgain());
     }
 
     private int readExercise() {
         return in.readIntValidatedWithCondition(
-                "Enter an exercise number (41-46, inclusive): ",
+                buildPrompt(),
                 "That is not a valid exercise number, please try again",
-                VALID_EXERCISE);
+                VALIDATE_EXERCISE);
     }
 
     private void runExercise(int exerciseNumber) {
-        String[] args = new String[0];
-
-        System.out.printf("Running exercise %d...%n%n", exerciseNumber);
-        switch (exerciseNumber) {
-            case 41 -> oop.exercises.ex41.Application.main(args);
-            case 42 -> oop.exercises.ex42.Application.main(args);
-            case 43 -> oop.exercises.ex43.Application.main(args);
-            case 44 -> oop.exercises.ex44.Application.main(args);
-            case 45 -> oop.exercises.ex45.Application.main(args);
-            case 46 -> oop.exercises.ex46.Application.main(args);
-            default -> throw new IllegalArgumentException("Exercise not valid");
+        try {
+            Method main = buildMainMethod(exerciseNumber);
+            runMainMethod(main, exerciseNumber);
+        } catch (ClassNotFoundException | NoSuchMethodException |
+                 IllegalAccessException | InvocationTargetException e) {
+            System.out.printf("Could not run exercise%n%s", e.getMessage());
+            e.printStackTrace();
         }
+    }
+
+    private boolean runAgain() {
+        String response = in.readString("Run another? (Y/N): ");
+        return response.matches("[Yy].*");
+    }
+
+    private Method buildMainMethod(int exerciseNumber) throws ClassNotFoundException, NoSuchMethodException {
+        String exercisePath = buildExercisePath(exerciseNumber);
+        Class<?> exercise = getExerciseClass(exercisePath);
+        return exercise.getDeclaredMethod("main", String[].class);
+    }
+
+    private void runMainMethod(Method main, int exerciseNumber) throws InvocationTargetException, IllegalAccessException {
+        System.out.printf("Running exercise %d...%n%n", exerciseNumber);
+        main.invoke(null, (Object[]) new String[1]);
+    }
+
+    private Class<?> getExerciseClass(String path) throws ClassNotFoundException {
+            return Class.forName(path);
+    }
+
+    private String buildExercisePath(int exerciseNumber) {
+        return String.format("oop.exercises.ex%d.Application", exerciseNumber);
+    }
+
+    private String buildPrompt() {
+        return String.format("Enter an exercise number (%d-%d, inclusive): ",
+                MIN_EXERCISE, MAX_EXERCISE);
     }
 }
