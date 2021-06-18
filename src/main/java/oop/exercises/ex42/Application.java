46692bf2e9b89658cc885c27a5425ed15a67de79
xera51 <xera51@protonmail.com>
2021-06-18 12:26:33 -0400

    Removed garbage test code

--- src/main/java/oop/exercises/ex42/Application.java
+++ src/main/java/oop/exercises/ex42/Application.java
@@ -4,33 +4,9 @@
  */
 package oop.exercises.ex42;
 
-import oop.exercises.util.FileHandler;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.*;
-import java.util.Enumeration;
-
-import static java.lang.ClassLoader.getSystemClassLoader;
-
 public class Application {
 
     public static void main(String[] args) {
-        try {
-            String filename = "exercise41_output.txt";
-            String output = "Test output.";
-            Path thePath = Paths.get(getSystemClassLoader().getResource(filename).toURI());
-            System.out.println(thePath);
-            Writer writer = Files.newBufferedWriter(thePath, StandardOpenOption.CREATE);
-            writer.write(output);
-            writer.close();
-        } catch (IOException | URISyntaxException e) {
-            System.out.println("error");
-        }
-
+        System.out.println("Exercise 42!");
     }
 }
