da35e3a11015ead5c7e9e1b8118c1b391af84bbc
xera51 <xera51@protonmail.com>
2021-06-18 12:27:14 -0400

    Now able to run all exercises in a row

--- src/main/java/oop/exercises/main/Runner.java
+++ src/main/java/oop/exercises/main/Runner.java
@@ -2,28 +2,28 @@
  *  UCF COP3330 Summer 2021 Assignment 3 Solution
  *  Copyright 2021 Christopher Gray
  */
+
 package oop.exercises.main;
 
-import oop.exercises.util.DataReader;
+import oop.exercises.util.ConsoleDataReader;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.function.IntPredicate;
 
-
 public class Runner {
 
     private static final int MIN_EXERCISE = 41;
     private static final int MAX_EXERCISE = 46;
-    private static final DataReader in = new DataReader();
+    private static final ConsoleDataReader in = new ConsoleDataReader();
     private static final IntPredicate VALIDATE_EXERCISE =
-            x -> MIN_EXERCISE <= x && x <= MAX_EXERCISE;
+            x -> (MIN_EXERCISE <= x && x <= MAX_EXERCISE) || x == -1;
 
     public static void main(String[] args) {
         Runner runner = new Runner();
         do {
             int exerciseNumber = runner.readExercise();
-            runner.runExercise(exerciseNumber);
+            runner.exerciseRunner(exerciseNumber);
         } while(runner.runAgain());
     }
 
@@ -34,13 +34,24 @@ public class Runner {
                 VALIDATE_EXERCISE);
     }
 
+    private void exerciseRunner(int exerciseNumber) {
+        if (exerciseNumber == -1) {
+            for(int i = MIN_EXERCISE; i <= MAX_EXERCISE; i++) {
+                runExercise(i);
+            }
+        } else {
+            runExercise(exerciseNumber);
+        }
+    }
+
+
     private void runExercise(int exerciseNumber) {
         try {
             Method main = buildMainMethod(exerciseNumber);
             runMainMethod(main, exerciseNumber);
         } catch (ClassNotFoundException | NoSuchMethodException |
                  IllegalAccessException | InvocationTargetException e) {
-            System.out.printf("Could not run exercise%n%s", e.getMessage());
+            System.out.printf("Could not run exercise%n%s%n", e.getMessage());
             e.printStackTrace();
         }
     }
@@ -70,7 +81,7 @@ public class Runner {
     }
 
     private String buildPrompt() {
-        return String.format("Enter an exercise number (%d-%d, inclusive): ",
+        return String.format("Enter an exercise number (%d-%d, inclusive), or -1 for all: ",
                 MIN_EXERCISE, MAX_EXERCISE);
     }
 }
